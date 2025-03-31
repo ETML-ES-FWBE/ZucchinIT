c9a9612fe343f36942ce7ac9c4c705c7b8f20000
Manueloro <oro.manuel@hotmail.com>
2025-03-31 22:09:15 +0200

    refactor: Better structure for security implement.

--- src/main/java/ch/etmles/payroll/extras/UptimeController.java
+++ src/main/java/ch/etmles/payroll/extras/UptimeController.java
@@ -1,5 +1,6 @@
 package ch.etmles.payroll;
 
+import jakarta.annotation.security.PermitAll;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +14,7 @@ import java.util.HashMap;
 
 @Controller
 @RequestMapping("/uptime")
+@PermitAll
 public class UptimeController {
 
     private Instant uptime;
