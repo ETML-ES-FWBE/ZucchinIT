d718d60c0fbaa6845647821306abbf68c5b297ef
Théo Mayoraz <theo.mayoraz@outlook.fr>
2025-03-31 10:23:46 +0200

    feat: add memberRole DTO

--- src/main/java/ch/etmles/payroll/memberRole/MemberRoleDTO.java
+++ src/main/java/ch/etmles/payroll/memberRole/MemberRoleDTO.java
@@ -1,2 +1,41 @@
-package ch.etmles.payroll.memberRole;public class MemberRoleDTO {
+package ch.etmles.payroll.memberRole;
+
+public class MemberRoleDTO {
+
+    private MemberRole.PlayerRole role;
+    private Long memberId;
+
+    private Long teamId;
+
+    public MemberRoleDTO() {}
+
+    public MemberRoleDTO(MemberRole.PlayerRole role, Long memberId, Long teamId) {
+        this.role = role;
+        this.memberId = memberId;
+        this.teamId = teamId;
+    }
+
+    public MemberRole.PlayerRole getRole() {
+        return role;
+    }
+
+    public void setRole(MemberRole.PlayerRole role) {
+        this.role = role;
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+
+    public Long getTeamId() {
+        return teamId;
+    }
+
+    public void setTeamId(Long teamId) {
+        this.teamId = teamId;
+    }
 }
