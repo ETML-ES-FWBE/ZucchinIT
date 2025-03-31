9cc1a7c2aea4adffafe19cf6c4ed522c4330dd3e
Manueloro <oro.manuel@hotmail.com>
2025-03-31 11:38:22 +0200

    feat: uptime route for healthcheck

---
+++ src/main/java/ch/etmles/payroll/UptimeController.java
@@ -0,0 +1,38 @@
+package ch.etmles.payroll;
+
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.HashMap;
+
+@Controller
+@RequestMapping("/uptime")
+public class UptimeController {
+
+    private Instant uptime;
+
+    @EventListener(ApplicationReadyEvent.class)
+    private void OnApplicationReady() {
+        this.uptime = Instant.now();
+    }
+
+    @GetMapping()
+    public ResponseEntity<Object> getUptime() {
+        if (uptime == null) return ResponseEntity.internalServerError().body(null);
+
+        Duration duration = Duration.between(uptime, Instant.now());
+        HashMap<String, String> data = new HashMap<>();
+        data.put("uptime", String.format("%d hours, %d minutes, %d seconds",
+                duration.toHours(),
+                duration.toMinutesPart(),
+                duration.toSecondsPart()));
+
+        return ResponseEntity.ok().body(data);
+    }
+}
